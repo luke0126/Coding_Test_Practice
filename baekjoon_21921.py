8cac6e896de5cd74216608d911a24fea130fe772
luke0126 <luke0126@naver.com>
2023-10-23 18:35:07 +0900

    백준 21921번 - 블로그

---
+++ baekjoon_21921.py
@@ -0,0 +1,33 @@
+# 백준 21921번 - 블로그
+
+import sys
+input = sys.stdin.readline
+
+n, x = map(int, input().split())
+info = list(map(int, input().split()))
+
+
+# 누적 방문자 수 구하기
+cum_visitors = [0]
+for i in range(n):
+    cum_visitors.append(cum_visitors[i] + info[i])
+
+print(cum_visitors)
+
+result = []
+day = 0
+
+# 현재 확인 중인 날부터 x일까지가 n일보다 작다면
+while day + x <= n:
+    # 누적합으로 이루어져 있기 때문에 x일 후와 현재 날짜 간의 방문자 수를 빼기
+    result.append(cum_visitors[day+x]-cum_visitors[day])
+    day += 1
+
+
+if sum(info) == 0:
+    print('SAD')
+else:
+    # 최대가 얼마인지
+    print(max(result))
+    # 최댓값과 같은 날이 얼마인지
+    print(result.count(max(result)))
